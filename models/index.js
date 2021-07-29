e8569e3a4410941f42f03dde53e61756bcaeb8b8
fanidt <fanidtu@gmail.com>
2021-07-29 12:44:27 -0700

    minor details fixing

--- models/index.js
+++ models/index.js
@@ -1,8 +1,12 @@
 const User = require("./User");
 const Post = require("./Post");
-const Vote = require("./Vote");
 const Comment = require("./Comment");
 const Pet = require("./Pet");
+
+// NOT SURE IF WE WILL USE
+const Vote = require('./Vote');
+
+
 
 // create associations
 User.hasMany(Post, {
@@ -28,22 +32,6 @@ Post.belongsToMany(User, {
   foreignKey: "post_id",
 });
 
-Vote.belongsTo(User, {
-  foreignKey: "user_id",
-});
-
-Vote.belongsTo(Post, {
-  foreignKey: "post_id",
-});
-
-User.hasMany(Vote, {
-  foreignKey: "user_id",
-});
-
-Post.hasMany(Vote, {
-  foreignKey: "post_id",
-});
-
 Comment.belongsTo(User, {
   foreignKey: "user_id",
 });
@@ -60,4 +48,30 @@ Post.hasMany(Comment, {
   foreignKey: "post_id",
 });
 
+User.hasMany(Pet, {
+  foreignKey: "user_id"
+})
+
+Pet.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+//NOT SURE ABOUT USING VOTE
+Vote.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
 module.exports = { User, Post, Vote, Comment, Pet };
