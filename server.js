d48b82b579c91182c15828b24326fe05e6c05f9b
fanidt <fanidtu@gmail.com>
2021-08-01 23:59:10 -0700

    images show but did not change anything on the routes or models, added some css, will continue working tomorrow night

--- server.js
+++ server.js
@@ -49,6 +49,9 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('public/images'))
+
+
 
 // turn on routes
 app.use(routes);
