33397628dce3e867bedd508e878b2ee66a9692bd
fanidt <fanidtu@gmail.com>
2021-08-02 17:03:09 -0700

    fixed css for the login and signup

--- models/User.js
+++ models/User.js
@@ -43,27 +43,17 @@ User.init(
   },
   {
     hooks: {
-      // set up beforeCreate lifecycle "hook" functionality
-      // STANDARD FUNCTION USING PROMISE
-      // beforeCreate(userData) {
-      //   return bcrypt.hash(userData.password, 10).then((newUserData) => {
-      //     return newUserData;
-      //   });
-      // },
-      // SAME FUNCTIONALITY USING ASYNC/AWAIT SYNTAX
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
-      // set up beforeUpdate lifecycle "hook" functionality
+
       async beforeUpdate(updatedUserData) {
-        updatedUserData.password = await bcrypt.hash(
-          updatedUserData.password,
-          10
-        );
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
         return updatedUserData;
-      },
+      }
     },
+    
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
