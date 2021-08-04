89dd752fe91227b1b04b85782bc955bbdc81648f
fanidt <fanidtu@gmail.com>
2021-08-03 21:15:26 -0700

    added another app.use

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static("public/images"));
+app.use(express.static("public/uploads"));
 
 // turn on routes
 app.use(routes);
