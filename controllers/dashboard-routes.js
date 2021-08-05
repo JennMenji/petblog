92099dab7f9d4bbacc916e5fb412dce05c44677f
fanidt <fanidtu@gmail.com>
2021-08-05 13:25:13 -0700

    fixed the user pets

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -35,7 +35,76 @@ router.get("/", withAuth, (req, res) => {
   });
 
 
+  router.get("/pets/:id", (req, res) => {
+    Pet.findAll({
+        where: {
+            id: req.session.user_id,
+          },
+      attributes: [
+        "id",
+        "name",
+        "age",
+        "animal",
+        "breed",
+        "dog_image",
+        "user_id",
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    })
+      .then((dbPostData) => {
+        const pets = dbPostData.map((pet) => pet.get({ plain: true }));
+        console.log(req.session);
+        // pass a single post object into the homepage template
   
+        console.log(pets);
+        res.render("dashboard", { pets, loggedIn: req.session.loggedIn });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+//To view a Pet Profile once you click on the Image
+router.get("/", (req, res) => {
+    Pet.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+      attributes: [
+        "id",
+        "name",
+        "animal",
+        "breed",
+        "age",
+        "dog_image",
+        "created_at",
+        "user_id",
+      ]
+     
+    })
+      .then((dbPetData) => {
+        if (!dbPetData) {
+          res.status(404).json({ message: "No pet found with this id " });
+          return;
+        }
+  
+        const pets = dbPostData.map((pet) => pet.get({ plain: true }));
+  
+        res.render("user-profile", { pet, loggedIn: req.session.loggedIn });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
 
 
 
